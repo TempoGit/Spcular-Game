51fe9bf4bdf0685ba7f1d989a2959d252a481bad
Gennaro Cirillo <joe.satriani.gc@gmail.com>
2022-02-22 17:32:11 +0100

    ojo

--- Specular-Game/GameScene.swift
+++ Specular-Game/GameScene.swift
@@ -145,7 +145,7 @@ class GameScene: SKScene {
         
         if(touchedNode.name == "playGameName"){
             musicHandler.instance.stopBackgroundMusicMenu()
-            let startGameScene = Level00_5(size: size)
+            let startGameScene = Level00(size: size)
             view?.presentScene(startGameScene)
         }
         if(touchedNode.name == "volumeOff"){
